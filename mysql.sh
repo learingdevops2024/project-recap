33579e95ff93e00f4dd5641b8c56a873a1760175
santhoshi <santhoshi6april@gmail.com>
2024-03-26 19:06:29 +0530

    space difference

--- mysql.sh
+++ mysql.sh
@@ -3,12 +3,12 @@ echo -e "\e[31m Installmysql server\e[0m"
 dnf install mysql-server -y
 echo $?
 echo "system start mysqld"
-echo -e"\e[31m start mysqld\e[0m"
+echo -e "\e[31m start mysqld\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
 echo $?
 echo "set password"
-echo -e"\e[31m set password\e[0m"
+echo -e "\e[31m set password\e[0m"
 
 mysql_secure_installation --set-root-pass ExpenseApp@1
 echo $?
