c1d3f68f7f5a28046f3780fd157c520a7d452c97
santhoshi <santhoshi6april@gmail.com>
2024-03-26 13:01:58 +0530

    frontend script..space

--- frondend.sh
+++ frondend.sh
@@ -1,21 +1,21 @@
-Print_Task_Heading"Install nginx"
+Print_Task_Heading "Install nginx"
 dnf install nginx -y
 echo $?
-Print_Task_Heading"system start nginx"
+Print_Task_Heading "system start nginx"
  systemctl enable nginx
 systemctl start nginx
 echo $?
 cp vim /etc/nginx/default.d/expense.conf
-Print_Task_Heading"Remove Default content"
+Print_Task_Heading "Remove Default content"
 rm -rf /usr/share/nginx/html/*
 echo $?
-Print_Task_Heading"Download frontend content"
+Print_Task_Heading "Download frontend content"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 echo $?
-Print_Task_Heading"Extract frontend content"
+Print_Task_Heading "Extract frontend content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 echo $?
-Print_Task_Heading"system restart"
+Print_Task_Heading "system restart"
 systemctl restart nginx
 echo $?
