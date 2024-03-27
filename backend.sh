ca643e828bd1f1a140e9d1f77aa8e1506abce765
santhoshi <santhoshi6april@gmail.com>
2024-03-28 04:39:30 +0530

    added service files...

--- backend.sh
+++ backend.sh
@@ -4,26 +4,32 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 echo $?
-cp backend.service /etc/systemd/system/backend.service
+
+
 echo "Add directory"
 echo -e "\e[32mAdd deirectory\e[0m"
 useradd expense
+cp backend.service /etc/systemd/system/backend.service
+rm -rf /app
 mkdir /app
 echo $?
+
 echo "application code"
 echo -e "\e[36mApplication code\e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
-echo $?
 unzip /tmp/backend.zip
+echo $?
+
+
 echo" dependencies"
 echo -e "\e[33mdependencies\e[0m"
 cd /app
 npm install
 echo $?
+
 echo " Expense Backend Service"
 echo -e "\e[32mbackend srvice\e[0m"
-
 echo $?
 
 echo "Load the service."
@@ -32,10 +38,12 @@ systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
 echo $?
+
 echo "Install mysql"
 echo -e "\e[35minstall mysql\e[0m"
 dnf install mysql -y
 echo $?
+
 echo "Load Schema"
 echo -e "\e[36m load schema\e[0m"
 mysql -h <172.31.94.46> -uroot -pExpenseApp@1 < /app/schema/backend.sql
