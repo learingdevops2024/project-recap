b4f2a9772d6f9dc652b0788b327a50aea5969c7e
santhoshi <santhoshi6april@gmail.com>
2024-03-28 17:04:40 +0530

    funtions

--- backend.sh
+++ backend.sh
@@ -1,6 +1,12 @@
 mysql_password- $1
 
-echo Install nodejs
+Print_Task_Heading () {
+ echo $1
+ echo "########$1##########" &>>/tmp/expence.log
+ }
+
+
+Print_Task_Heading "Install nodejs"
 echo -e "\e[31m Installnodejs server\e[0m"
 dnf module disable nodejs -y &>>/tmp/expence.log
 dnf module enable nodejs:20 -y &>>/tmp/expence.log
@@ -8,7 +14,7 @@ dnf install nodejs -y &>>/tmp/expence.log
 echo $?
 
 
-echo Add directory
+Print_Task_Heading "Add directory"
 echo -e "\e[32mAdd directory\e[0m"
 useradd expense &>>/tmp/expence.log
 cp backend.service /etc/systemd/system/backend.service file
@@ -16,7 +22,7 @@ rm -rf /app &>>/tmp/expence.log
 mkdir /app &>>/tmp/expence.log
 echo $?
 
-echo application code
+Print_Task_Heading "application code"
 echo -e "\e[36mApplication code\e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expence.log
 cd /app &>>/tmp/expence.log
@@ -24,30 +30,30 @@ unzip /tmp/backend.zip &>>/tmp/expence.log
 echo $?
 
 
-echo dependencies
+Print_Task_Heading "dependencies"
 echo -e "\e[33mdependencies\e[0m"
 cd /app &>>/tmp/expence.log
 npm install &>>/tmp/expence.log
 echo $?
  cp vim /etc/systemd/system/backend.service
 
-echo Load the service.
+Print_Task_Heading "Load the service"
 echo -e "\e[34mload the service\e[0m"
 systemctl daemon-reload &>>/tmp/expence.log
 echo $?
 
-echo system start
+Print_Task_Heading "system start"
 systemctl enable backend &>>/tmp/expence.log
 systemctl start backend &>>/tmp/expence.log
 echo $?
 
 
-echo Install mysql
+Print_Task_Heading "Install mysql"
 echo -e "\e[35minstall mysql\e[0m"
 dnf install mysql -y &>>/tmp/expence.log
 echo $?
 
-echo Load Schema
+Print_Task_Heading "Load Schema"
 echo -e "\e[36m load schema\e[0m"
 mysql -h <172.31.94.46> -uroot -p${mysql_password} < /app/schema/backend.sql &>>/tmp/expence.log
 echo $?
