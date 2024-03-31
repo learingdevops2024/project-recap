c85789be8dc087cb21605f972ef4c6d4b0537358
santhoshi <santhoshi6april@gmail.com>
2024-03-31 09:41:54 +0530

    status of every operation success or failure

--- backend.sh
+++ backend.sh
@@ -1,5 +1,5 @@
 source common.sh
-mysql_password - $1
+mysql_password= $1
 
 # if password not provided then we will exit
 
@@ -13,7 +13,7 @@ echo -e "\e[31m Installnodejs server\e[0m"
 dnf module disable nodejs -y &>>/tmp/expence.log
 dnf module enable nodejs:20 -y &>>/tmp/expence.log
 dnf install nodejs -y &>>/tmp/expence.log
-echo $?
+check_status $?
 
 
 Print_Task_Heading "Add directory"
@@ -22,40 +22,38 @@ useradd expense &>>/tmp/expence.log
 cp backend.service /etc/systemd/system/backend.service file
 rm -rf /app &>>/tmp/expence.log
 mkdir /app &>>/tmp/expence.log
-echo $?
+check_status $?
 
 Print_Task_Heading "application code"
 echo -e "\e[36mApplication code\e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expence.log
 cd /app &>>/tmp/expence.log
 unzip /tmp/backend.zip &>>/tmp/expence.log
-echo $?
+check_status $?
 
 
 Print_Task_Heading "dependencies"
 echo -e "\e[33mdependencies\e[0m"
 cd /app &>>/tmp/expence.log
 npm install &>>/tmp/expence.log
-echo $?
+check_status $?
  cp vim /etc/systemd/system/backend.service
 
 Print_Task_Heading "Load the service"
 echo -e "\e[34mload the service\e[0m"
 systemctl daemon-reload &>>/tmp/expence.log
-echo $?
-
+check_status $?
 Print_Task_Heading "system start"
 systemctl enable backend &>>/tmp/expence.log
 systemctl start backend &>>/tmp/expence.log
-echo $?
-
+check_status $?
 
 Print_Task_Heading "Install mysql"
 echo -e "\e[35minstall mysql\e[0m"
 dnf install mysql -y &>>/tmp/expence.log
-echo $?
+check_status $?
 
 Print_Task_Heading "Load Schema"
 echo -e "\e[36m load schema\e[0m"
 mysql -h <172.31.94.46> -uroot -p${mysql_password} < /app/schema/backend.sql &>>/tmp/expence.log
-echo $?
+check_status $?
--- common.sh
+++ common.sh
@@ -2,14 +2,13 @@ Print_Task_Heading () {
  echo $1
  echo "########$1##########" &>>/tmp/expence.log
  }
+
 check_status () {
-  if [$1 -eq 0]; then
+  if [ $1 -eq 0]; then
     echo -e "\e[32mSUCCESS\e[0m"
     else
-      echo -e "\e[31m FAILURE\E[0m"
+    echo -e "\e[31m FAILURE\E[0m"
       exit2
       fi
 }
 
-&>>/tmp/expence.log
-$LOG =/tmp/expence.log
