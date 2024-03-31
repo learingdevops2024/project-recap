4b4a4809ab17475217a123e5ced79316eff10437
santhoshi <santhoshi6april@gmail.com>
2024-03-31 10:48:51 +0530

    Adding check status to all the scripts.

--- common.sh
+++ common.sh
@@ -7,9 +7,9 @@ Print_Task_Heading () {
  check_status() {
 if [ $1 -eq 0]; then
     echo -e "\e[32mSUCCESS\e[0m"
-    else
-    echo -e "\e[31m FAILURE\E[0m"
+else
+    echo -e "\e[31m FAILURE\e[0m"
      exit 2
-      fi
+fi
 }
 
